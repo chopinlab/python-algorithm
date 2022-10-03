0a78b4bf4cda25b36b84130132f3969357279d8d
chopinfrog <bespingglobal@gmail.com>
2022-10-03 16:21:11 +0900

    #7 greedy_5585.py
    1. greedy_5585.py

--- practice/baekjun/algorithm/greedy_5585.py
+++ practice/baekjun/algorithm/greedy_5585.py
@@ -1,12 +1,23 @@
 """
 greedy
 https://www.acmicpc.net/problem/5585
-"""
-
-
+거스름돈
 """
 
 """
+380   # 4
+1     # 15 
+"""
+
+change = 1000 - int(input())
+count = 0
+coin_types = [500, 100, 50, 10, 5, 1]
+
+for coin in coin_types:
+    count += change // coin
+    change %= coin
+
+print(count)
 
 
 
